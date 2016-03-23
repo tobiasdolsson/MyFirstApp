510ad72098018ba067f15f523c54a404930b2132
tobiasdolsson <tobias.d.olsson@hotmail.com>
2016-03-23 19:10:10 +0100

    added direction text and lowpass filter

--- app/src/main/java/com/example/tobias/test/CompassActivity.java
+++ app/src/main/java/com/example/tobias/test/CompassActivity.java
@@ -10,6 +10,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class CompassActivity extends AppCompatActivity implements SensorEventListener{
@@ -18,11 +19,13 @@ public class CompassActivity extends AppCompatActivity implements SensorEventLis
     private SensorManager mSensorManager;
     Sensor accelerometer;
     Sensor magnetometer;
-    float[] mGravity;
-    float[] mGeomagnetic;
+    float[] mGravity = new float[3];
+    float[] mGeomagnetic = new float[3];
     float Ro[];
     float I[];
     private TextView degrees;
+    private TextView direction;
+    private ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +37,7 @@ public class CompassActivity extends AppCompatActivity implements SensorEventLis
         Ro = new float[9];
         I = new float[9];
         degrees=(TextView) findViewById(R.id.azimut);
+        direction=(TextView) findViewById(R.id.direction);
         mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
         accelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         magnetometer = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
@@ -53,11 +57,17 @@ public class CompassActivity extends AppCompatActivity implements SensorEventLis
     @Override
     public void onSensorChanged(SensorEvent event) {
         if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER){
-            mGravity = event.values;
+            final float alpha = 0.8f;
+            mGravity[0] = alpha * mGravity[0] + (1 - alpha) * event.values[0];
+            mGravity[1] = alpha * mGravity[1] + (1 - alpha) * event.values[1];
+            mGravity[2] = alpha * mGravity[2] + (1 - alpha) * event.values[2];
         }
 
         if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD){
-            mGeomagnetic = event.values;
+            final float alpha = 0.8f;
+            mGeomagnetic[0] = alpha * mGeomagnetic[0] + (1 - alpha) * event.values[0];
+            mGeomagnetic[1] = alpha * mGeomagnetic[1] + (1 - alpha) * event.values[1];
+            mGeomagnetic[2] = alpha * mGeomagnetic[2] + (1 - alpha) * event.values[2];
         }
 
         if(mGravity != null && mGeomagnetic != null){
@@ -67,8 +77,27 @@ public class CompassActivity extends AppCompatActivity implements SensorEventLis
                 float orientation[] = new float[3];
                 SensorManager.getOrientation(Ro, orientation);
                 azimut = Math.toDegrees(orientation[0]);
-
-                degrees.setText(String.valueOf(azimut));
+                if (azimut < 0) {
+                    azimut += 360;
+                }
+                int range = (int) (azimut / (360f / 16f));
+                if (range == 15 || range == 0)
+                    direction.setText("NORTH");
+                if (range == 1 || range == 2)
+                    direction.setText("NORTHEAST");
+                if (range == 3 || range == 4)
+                    direction.setText("EAST");
+                if (range == 5 || range == 6)
+                    direction.setText("SOUTHEAST");
+                if (range == 7 || range == 8)
+                    direction.setText("SOUTH");
+                if (range == 9 || range == 10)
+                    direction.setText("SOUTHWEST");
+                if (range == 11 || range == 12)
+                    direction.setText("WEST");
+                if (range == 13 || range == 14)
+                    direction.setText("NORTHWEST");
+                degrees.setText(String.format("%.0f", azimut)+((char) 176));
             }
 
         }
